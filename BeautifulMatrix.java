89ef4eb7841c0333efce147edc4d88a55174410d
Anjali853 <anjalianjali06149@gmail.com>
2026-05-22 00:57:13 +0530

    Petya and Strings

---
+++ BeautifulMatrix.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BeautifulMatrix {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int [][]matrix = new int[5][5];
+    for(int i =0 ; i<5; i++){
+        for(int j = 0; j<5; j++){
+            matrix[i][j] = sc.nextInt();
+        }
+    }
+    int row = 0;
+    int col = 0; 
+    for(int i = 0; i<5; i++){
+        for(int j = 0; j<5; j++){
+            if(matrix[i][j] == 1){
+                row = i;
+                col = j;
+                break;
+            }
+        }
+    }
+    System.out.println(Math.abs(col - 2) + Math.abs(row - 2));
+}
+}
