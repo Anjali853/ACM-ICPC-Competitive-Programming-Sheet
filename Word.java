840d5062f5a82a67a5a2f5e2d2c3898ae6a5ff3f
Anjali853 <anjalianjali06149@gmail.com>
2026-06-02 15:46:27 +0530

    prb

---
+++ Word.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Word {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String hello = "hello";
+
+        int j = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (j < hello.length() && s.charAt(i) == hello.charAt(j)) {
+                j++;
+            }
+        }
+
+        if (j == hello.length()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
