9adc0725816850ff64877997764f16c147d35824
Anjali853 <anjalianjali06149@gmail.com>
2026-06-07 15:20:34 +0530

    Police Recruits

---
+++ PoliceRecruits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PoliceRecruits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       int police = 0;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            if (x > 0) {
+                police += x;
+            } else {
+                if (police > 0) {
+                    police--;
+                } else {
+                    result++;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+    
+}
--- Stones.java
+++ Stones.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
-public class StonesOnTheTable {
-    public static void main(String[] args) {
+public class Stones {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         String s = sc.next();
-
         int count = 0;
-
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < s.length(); i++) {
             if (s.charAt(i) == s.charAt(i - 1)) {
                 count++;
             }
         }
-
         System.out.println(count);
-        sc.close();
+  
     }
+    
 }
