f3f909a8d608f67f7b496fea2e513053ae1c2ef0
Anjali853 <anjalianjali06149@gmail.com>
2026-07-19 23:58:48 +0530

    ACCepted

--- AcCepted.java
+++ AcCepted.java
@@ -4,15 +4,34 @@ public class AcCepted {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isUpperCase(c)) {
-                if (s.charAt(0) != 'A') {
+
+        if (s.charAt(0) != 'A') {
+            System.out.println("WA");
+            return;
+        }
+
+        int count = 0;
+        for (int i = 2; i <= s.length() - 2; i++) {
+            if (s.charAt(i) == 'C') {
+                count++;
+            }
+        }
+
+        if (count != 1) {
+            System.out.println("WA");
+            return;
+        }
+        for (int i = 1; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                if (ch != 'C') {
                     System.out.println("WA");
                     return;
                 }
             }
         }
-        System.out.println("Accepted");
+
+        System.out.println("AC");
     }
 }
