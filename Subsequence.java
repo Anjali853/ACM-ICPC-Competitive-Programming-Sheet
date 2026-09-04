c15d3c3ee9972168a2f0ab7a77fad1ba020658b4
Anjali853 <anjalianjali06149@gmail.com>
2026-09-05 01:33:59 +0530

    Mahmoud and Longest Uncommon Subsequence

---
+++ Subsequence.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Subsequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        if (a.equals(b)) {
+            System.out.println(-1);
+        } else {
+            System.out.println(Math.max(a.length(), b.length()));
+        }
+    }
+}
